8b80aab425b2c9b67c278db7e1b1d476fcb9bf6b
Khang Pham <khangphama@gmail.com>
2020-08-20 06:47:58 -0700

    add sample code

---
+++ sample/backprop.py
@@ -0,0 +1,26 @@
+#source https://towardsdatascience.com/lets-code-a-neural-network-in-plain-numpy-ae7e74410795
+def full_backward_propagation(Y_hat, Y, memory, params_values, nn_architecture):
+    grads_values = {}
+    m = Y.shape[1]
+    Y = Y.reshape(Y_hat.shape)
+   
+    dA_prev = - (np.divide(Y, Y_hat) - np.divide(1 - Y, 1 - Y_hat));
+    
+    for layer_idx_prev, layer in reversed(list(enumerate(nn_architecture))):
+        layer_idx_curr = layer_idx_prev + 1
+        activ_function_curr = layer["activation"]
+        
+        dA_curr = dA_prev
+        
+        A_prev = memory["A" + str(layer_idx_prev)]
+        Z_curr = memory["Z" + str(layer_idx_curr)]
+        W_curr = params_values["W" + str(layer_idx_curr)]
+        b_curr = params_values["b" + str(layer_idx_curr)]
+        
+        dA_prev, dW_curr, db_curr = single_layer_backward_propagation(
+            dA_curr, W_curr, b_curr, Z_curr, A_prev, activ_function_curr)
+        
+        grads_values["dW" + str(layer_idx_curr)] = dW_curr
+        grads_values["db" + str(layer_idx_curr)] = db_curr
+    
+    return grads_values
